0b4e79b42d007860ed79220e6eac20c5c31243fc
pavel <pavel@example.com>
2022-08-31 19:19:48 +0300

    deleted test variant

--- src/ConstructorPageTest/constants.ts
+++
@@ -1,3 +0,0 @@
-export const ItemTypes = {
-    SCHEMATIC_FIELD: 'schematic field'
-}
--- src/ConstructorPageTest/types.ts
+++
@@ -1,5 +0,0 @@
-export interface SchematicFieldItem {
-    id: number;
-    title: string;
-    type: string;
-}
