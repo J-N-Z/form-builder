57c1328572bd26961d6f2f405babf1169f587f1a
pavel <pavel@example.com>
2022-09-01 14:46:31 +0300

    added table to DBTable page

--- src/ConstructorPage/mock.ts
+++ src/ConstructorPage/mock.ts
@@ -4,51 +4,61 @@ export const availableFieldsDefault: SchematicFieldItem[] = [
     {
         id: 1,
         title: 'HDD, всего Гб',
-        type: 'целое число'
+        type: 'целое число',
+        sqlName: 'iTotalHDDSize'
     },
     {
         id: 2,
         title: 'Host name',
-        type: 'строка'
+        type: 'строка',
+        sqlName: 'sHostName'
     },
     {
         id: 3,
         title: 'IP-адрес',
-        type: 'строка'
+        type: 'строка',
+        sqlName: 'sIPAddr'
     },
     {
         id: 4,
         title: 'MAC-адрес',
-        type: 'строка'
+        type: 'строка',
+        sqlName: 'sMACAddr'
     },
     {
         id: 5,
         title: 'RAM, Мб',
-        type: 'целое число'
+        type: 'целое число',
+        sqlName: 'iRAMSize'
     },
     {
         id: 6,
         title: 'В домене',
-        type: 'да/нет'
+        type: 'да/нет',
+        sqlName: 'bInDomain'
     },
     {
         id: 7,
         title: 'В сети',
-        type: 'да/нет'
+        type: 'да/нет',
+        sqlName: 'bOnline'
     },
     {
         id: 8,
         title: 'Варианты выбытия',
-        type: 'выпадающий список'
+        type: 'выпадающий список',
+        sqlName: 'seDisposalOption'
     },
     {
         id: 9,
         title: 'Включая налог (%)',
-        type: 'десятичное число'
+        type: 'десятичное число',
+        sqlName: 'fTaxValue'
     },
     {
         id: 10,
         title: 'Внешний ID',
-        type: 'строка'
+        type: 'строка',
+        sqlName: 'sExternalId'
     },
 ];
--- src/DBTablePage/DBTablePage.tsx
+++ src/DBTablePage/DBTablePage.tsx
@@ -1,6 +1,47 @@
-export const DBTablePage = () => {
+import { Table } from '@admiral-ds/react-ui';
+import { availableFieldsDefault } from '../ConstructorPage/mock';
 
+
+const columnList = [
+    {
+        name: "fieldName",
+        title: "Название",
+        width: 350 
+    },
+    {
+        name: "SQLName",
+        title: "SQL name",
+        width: 250
+    },
+    {
+        name: "fieldType",
+        title: "Тип",
+        width: 140
+    },
+    {
+        name: "fieldExtra",
+        title: "Дополнительно",
+        width: 340
+    },
+];
+
+const rowList = availableFieldsDefault.map((item) => ({
+    id: item.id,
+    fieldName: item.title,
+    SQLName: item.sqlName,
+    fieldType: item.type,
+    fieldExtra: ''
+}));
+
+
+export const DBTablePage = () => {
     return (
-        <div>DBTablePage</div>
+        <div>
+            <Table
+                rowList={rowList}
+                columnList={columnList}
+                greyHeader={true}
+            />
+        </div>
     )
 }
